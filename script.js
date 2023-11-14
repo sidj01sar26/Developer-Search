520c7e64e34f800d408d410a96ca384d9815142e
Siddharth Joisar <sidjoisar0807@gmail.com>
2023-11-15 01:01:09 +0530

    dark mode and final change

--- script.js
+++ script.js
@@ -130,4 +130,62 @@ function updateProfile(data) {
     }
 }
 
+//SWITCH TO DARK MODE - activateDarkMode()
+function darkModeProperties() {
+    root.setProperty("--lm-bg", "#141D2F");
+    root.setProperty("--lm-bg-content", "#1E2A47");
+    root.setProperty("--lm-text", "white");
+    root.setProperty("--lm-text-alt", "white");
+    root.setProperty("--lm-shadow-xl", "rgba(70,88,109,0.15)");
+    modetext.innerText = "LIGHT";
+    modeicon.src = "./assets/images/sun-icon.svg";
+    root.setProperty("--lm-icon-bg", "brightness(1000%)");
+    darkMode = true;
+    console.log("darkmode changed to " + darkMode);
+    localStorage.setItem("dark-mode", true);
+    console.log("setting dark mode to false");
+
+    console.log("setting dark mode to true");
+}
+
+//SWITCH TO LIGHT MODE - activateLightMode()
+function lightModeProperties() {
+    root.setProperty("--lm-bg", "#F6F8FF");
+    root.setProperty("--lm-bg-content", "#FEFEFE");
+    root.setProperty("--lm-text", "#4B6A9B");
+    root.setProperty("--lm-text-alt", "#2B3442");
+    root.setProperty("--lm-shadow-xl", "rgba(70, 88, 109, 0.25)");
+    modetext.innerText = "DARK";
+    modeicon.src = "./assets/images/moon-icon.svg";
+    root.setProperty("--lm-icon-bg", "brightness(100%)");
+    darkMode = false;
+    console.log("darkmode changed to " + darkMode);
+
+    localStorage.setItem("dark-mode", false);
+    console.log("setting dark mode to false");
+}
+
+//INITIALISE UI
+function init() {
+    //initialise dark-mode variable to false;
+    darkMode = false;
+
+    const value = localStorage.getItem("dark-mode");
+
+    if (value === null) {
+        console.log("null k andar");
+        localStorage.setItem("dark-mode", darkMode);
+        // lightModeProperties();  
+        darkModeProperties();
+    } else if (value == "true") {
+        console.log("truer k andar");
+        darkModeProperties();
+    } else if (value == "false") {
+        console.log("false k andar");
+        lightModeProperties();
+    }
+
+    getUserData(url + "sidj01sar26");
+}
+
 init();
